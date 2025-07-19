faef5c6992effd73e393e9ccaa4ed0f847ad1b2e
angaddubey01 <mar23angad@gmail.com>
2025-07-19 10:21:54 +0530

    B_YashpalTest1

--- sympy/simplify/fu.py
+++ sympy/simplify/fu.py
@@ -501,9 +501,9 @@ def _TR56(rv, f, g, h, max, pow):
         if not (rv.is_Pow and rv.base.func == f):
             return rv
 
-        if (rv.exp < 0) == True:
+        if rv.exp.is_real and (rv.exp < 0) == True:
             return rv
-        if (rv.exp > max) == True:
+        if rv.exp.is_real and (rv.exp > max) == True:
             return rv
         if rv.exp == 2:
             return h(g(rv.base.args[0])**2)
