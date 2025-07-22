d3a23a3e4a4f91ca1771ea49d69da0a7817d12ca
angaddubey01 <mar23angad@gmail.com>
2025-07-22 09:23:27 +0530

    B12_29_24539

--- sympy/polys/tests/test_rings.py
+++ sympy/polys/tests/test_rings.py
@@ -259,13 +259,12 @@ def test_PolyElement_as_expr():
     assert f != g
     assert f.as_expr() == g
 
-    X, Y, Z = symbols("x,y,z")
-    g = 3*X**2*Y - X*Y*Z + 7*Z**3 + 1
+    U, V, W = symbols("u,v,w")
+    g = 3*U**2*V - U*V*W + 7*W**3 + 1
 
     assert f != g
-    assert f.as_expr(X, Y, Z) == g
-
-    raises(ValueError, lambda: f.as_expr(X))
+    assert f.as_expr(U, V, W) == g
+    raises(ValueError, lambda: f.as_expr(U))
 
     R, = ring("", ZZ)
     assert R(3).as_expr() == 3
